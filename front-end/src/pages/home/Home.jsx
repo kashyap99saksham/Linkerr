1c6b2c08a4c7501c57d15b72a0f3f4e34c287ca7
kashyap99saksham <2017kucp1056@iiitkota.ac.in>
2022-07-15 22:31:12 +0530

    front-end v1

---
+++ front-end/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PostSide from '../../components/PostSide/PostSide'
+import ProfileSide from '../../components/profileSide/ProfileSide'
+import RightSide from '../../components/RightSide/RideSide'
+import './Home.css'
+const Home = () => {
+  return (
+    <div className="Home">
+        <ProfileSide/>
+        <PostSide/>
+        <RightSide/>
+    </div>
+  )
+}
+
+export default Home
